8f8ff99c7f1db24e2716db5724374e4d7a0490fb
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-11-27 18:59:40 -0500

    BaseGame and MEH Code

---
+++ Assets/Scripts/FollowMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMovement : MonoBehaviour {
+
+    [SerializeField] bool targetIsRadius = false;
+    public float limitUp    =    5;
+    public float limitDown  =    -5;
+    public float limitLeft  =    -5;
+    public float limitRight =    5;
+    public float speed = 3;
+    public float offSetX = 0;
+    public float offSetY = 2;
+    public float offSetZ = -10;
+
+    [SerializeField] int randoTimer = 10;
+
+    GameObject player;
+    EnemyAdvanceMovement eam;
+    Vector3 rando = Vector3.zero;
+    int useRando = 0;
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(GetComponentInChildren<EnemyAdvanceMovement>())
+        {
+            eam = GetComponentInChildren<EnemyAdvanceMovement>();
+        }
+	}
+	
+	void FixedUpdate ()
+    {
+        Vector3 target;
+
+        if (targetIsRadius)
+        {
+            target = (transform.position - player.transform.position).normalized * offSetX;
+            target.y = Mathf.Abs(target.y);
+            target.y += offSetY;
+            target = player.transform.position + target;
+        }
+        else
+        {
+            target = player.transform.position;
+            target.x += offSetX;
+            target.y += offSetY;
+            target.z = offSetZ;
+        }
+        if (limitUp == 0 && limitUp == limitDown && limitRight == limitLeft)
+        {
+
+        }
+        else
+        {
+            if (transform.position.y > limitUp && player.transform.position.y > limitUp)
+            {
+                target.y = limitUp;
+            }
+            if (transform.position.y < limitDown && player.transform.position.y < limitDown)
+            {
+                target.y = limitDown;
+            }
+
+            if (transform.position.x > limitRight && player.transform.position.x > limitRight)
+            {
+                target.x = limitRight;
+            }
+            if (transform.position.x < limitLeft && player.transform.position.x < limitLeft)
+            {
+                target.x = limitLeft;
+            }
+        }
+        if (!eam || !eam.attacking)
+        {
+            if (eam && useRando >= randoTimer)
+            {
+                rando = new Vector3((Random.value - .5f) * 3, (Random.value - .5f) * 3, 0);
+                useRando = 0;
+            }
+            transform.position = Vector3.Lerp(transform.position, target + rando, speed * Time.fixedDeltaTime);
+            useRando++;
+        }
+    }
+}
---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour {
+
+    public float Lup = 0;
+    public float Ldown = 0;
+    public float Lright = 0;
+    public float Lleft = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerMovement>())
+        {
+            FollowMovement cms = GameObject.Find("Camera").GetComponent<FollowMovement>();
+            cms.limitUp = Lup;
+            cms.limitDown = Ldown;
+            cms.limitRight = Lright;
+            cms.limitLeft = Lleft;
+        }
+    }
+
+
+}
