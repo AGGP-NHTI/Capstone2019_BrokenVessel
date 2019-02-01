757d5d66c7a2dbc2537e9b7dafe71759b7bbe835
killswitchq <bsotherden@gmail.com>
2019-02-01 14:58:22 -0500

    Inventory
    Inventory, item pickups, and my test scene to work with

---
+++ Assets/Scripts/ItemScripts/InventoryScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour {
+    public GameObject[] hotBarItems = new GameObject[3];
+    public GameObject[] inventoryStorage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
