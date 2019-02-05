4f5dbc4cf4ac6f765495d5d62f993ede5acf0d30
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2019-02-05 14:18:00 -0500

    base enemy push

---
+++ Assets/Scripts/EnemyAdvanceMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAdvanceMovement : MonoBehaviour
+{
+
+    [SerializeField] bool targetIsRadius = false;
+    public float speed = 5;
+    public float offSetX = 0;
+    public float offSetY = 2;
+    public float offSetZ = 0;
+
+    public bool attacking = false;
+    int timer = 0;
+    [SerializeField] int timerMax = 10;
+
+    public Transform homePlate;
+    Transform player;
+    Vector3 lastPlayerPosition = Vector3.zero;
+    Vector3 target = Vector3.one * 5;
+    [SerializeField] Transform pivot;
+    [SerializeField] Transform orbitPoint;
+
+    Rigidbody2D rig;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (targetIsRadius)
+        {
+            target = (transform.position - lastPlayerPosition).normalized * offSetX;
+            target.y = Mathf.Abs(target.y);
+            target += lastPlayerPosition;
+            target.y += offSetY;
+        }
+        else
+        {
+            target = lastPlayerPosition;
+            target.x += offSetX;
+            target.y += offSetY;
+            target.z = offSetZ;
+        }
+        pivot.Rotate(Vector3.forward, 180 * Time.fixedDeltaTime);
+
+        if (!attacking)
+        {
+            timer++;
+            rig.velocity = Vector2.zero;
+            transform.position = Vector3.MoveTowards(transform.position, orbitPoint.position, speed * Time.fixedDeltaTime);
+            //rig.AddForce((orbitPoint.position - transform.position).normalized * speed * 100 * Time.fixedDeltaTime);
+            if (timer >= timerMax)
+            {
+                timer = 0;
+                attacking = true;
+                lastPlayerPosition = player.position;
+                
+                rig.velocity = Vector2.zero;
+            }
+        }
+        if (attacking)
+        {
+            Attack();
+        }
+    }
+
+    public virtual void Attack()
+    {
+        timer++;
+        //transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
+        rig.AddForce((target - transform.position).normalized * speed * 100 * Time.fixedDeltaTime);
+        if (timer >= timerMax / 2)
+        {
+            timer = 0;
+            attacking = false;
+            rig.velocity = Vector2.zero;
+            if (Vector3.Distance(player.position, orbitPoint.position) < Vector3.Distance(transform.position, orbitPoint.position))
+            {
+                homePlate.position = transform.position;
+                transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 5;
+
+    public bool followLedge = false;
+    public bool bounceOff = false;
+    public bool moveOnJump = false;
+    public bool activeIfPlayer = false;
+    public enum DetectionType {none, ray, circle, box};
+    public DetectionType choice = DetectionType.none;
+    public float range = 10f;
+
+    public bool hitHead = false;
+    bool onLedge = false;
+    bool grounded = true;
+    bool isJumping = false;
+    int timer = 0;
+    bool seePlayer = false;
+
+    public Transform faceCheck;
+    public Transform ledgeCheck;
+    public LayerMask realGround;
+    public LayerMask target;
+
+    Rigidbody2D rig;
+
+    void Start ()
+    {
+        rig = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update()
+    {
+        grounded = Physics2D.CircleCast(ledgeCheck.position, .2f, Vector2.zero, 0, realGround);
+        onLedge = !Physics2D.CircleCast(ledgeCheck.position, .075f, Vector2.zero, 0, realGround);
+
+        if (followLedge && grounded)
+        {
+            rig.gravityScale = 0;
+            rig.freezeRotation = true;
+        }
+        else
+        {
+            rig.gravityScale = 1;
+            rig.freezeRotation = false;
+        }
+        if(bounceOff && (hitHead || onLedge) && !moveOnJump)
+        {
+            Flip();
+            speed = -speed;
+        }
+        if(bounceOff && moveOnJump && hitHead)
+        {
+            Flip();
+            speed = -speed;
+        }
+        if (!grounded && !moveOnJump)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            RaycastHit2D hit = Physics2D.Raycast(ledgeCheck.position, Vector2.down, .3f, realGround);
+            if(hit)
+            {
+                transform.position = new Vector3(transform.position.x, hit.transform.position.y);
+            }
+        }
+        else if (followLedge && hitHead)
+        {
+            rig.isKinematic = true;
+            transform.position = faceCheck.position;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.z + (90 * transform.localScale.x)));
+            rig.isKinematic = false;
+        }
+        hitHead = false;
+    }
+    private void FixedUpdate()
+    {
+        switch (choice)
+        {
+            case DetectionType.none:
+                seePlayer = true;
+                break;
+            case DetectionType.ray:
+                seePlayer = Physics2D.Raycast(faceCheck.position, Vector2.right * transform.localScale.x, range, target);
+                Debug.DrawRay(faceCheck.position, Vector2.right * transform.localScale.x, Color.red, range);
+                break;
+            case DetectionType.circle:
+                seePlayer = Physics2D.CircleCast(transform.position, range, Vector2.zero, 0, target);
+                break;
+            case DetectionType.box:
+                Debug.Log("ERROR: NOT ADDED");
+                break;
+        }
+
+        if (seePlayer)
+        {
+            if (moveOnJump)
+            {
+                if (!onLedge)
+                {
+                    isJumping = true;
+                }
+                if(isJumping && timer >= 80)
+                {
+                    rig.AddForce(transform.up * Mathf.Abs(speed) * 450, ForceMode2D.Force);
+                    rig.AddForce(transform.right * speed * 150);
+                }
+                if (timer >= 90)
+                {
+                    rig.AddForce(transform.up * -speed * 500, ForceMode2D.Force);
+                    timer = 0;
+                    isJumping = false;
+                }
+                timer++;
+            }
+            else
+            {
+                if (!onLedge || !followLedge)
+                {
+                    rig.AddForce(transform.right * speed * 100, ForceMode2D.Force);
+                }
+                else if (grounded)
+                {
+                    transform.Rotate(new Vector3(0, 0, -10 * transform.localScale.x));
+                    rig.velocity = Vector3.zero;
+                }
+            }
+        }
+        seePlayer = false;
+    }
+
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
