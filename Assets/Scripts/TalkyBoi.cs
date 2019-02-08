057c10fc46c2a4756ec9a787f1d827e1e252f8e1
Hyperion47 <37087822+Hyperion47@users.noreply.github.com>
2019-02-08 13:49:12 -0500

    DialogueWork
    added a new font, added distance checking, tested them in my scene

---
+++ Assets/Scripts/TalkyBoi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkyBoi : MonoBehaviour {
+
+    public GameObject Cube1;
+    public GameObject Cube2;
+    public float Distance_;
+    public bool textBoxVisible = false;
+    public GameObject textBox;
+    public GameObject textBoxInstance;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Distance_ = Vector3.Distance(Cube1.transform.position, Cube2.transform.position);
+        if(Distance_ < 7)
+        {
+            Debug.Log("ALERT ENEMY");
+            if(textBoxVisible == false)
+            {
+                textBoxInstance = Instantiate(textBox);
+                textBoxVisible = true;
+            }
+        }
+        else
+        {
+            if (textBoxVisible == true)
+            {
+                Destroy(textBoxInstance);
+                textBoxVisible = false;
+            }
+        }
+	}
+}
