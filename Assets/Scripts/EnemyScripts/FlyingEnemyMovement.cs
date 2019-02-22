538d97e0e89b234d1c515067b32cb5ebe5999398
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2019-02-22 14:43:16 -0500

    fly enemy update

--- Assets/Scripts/EnemyScripts/FlyingEnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/FlyingEnemyMovement.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class FlyingEnemyMovement : MonoBehaviour {
 
     [SerializeField] bool approachPlayer;
+    [SerializeField] float approachDistance = 2;
+
+    Transform target;
+    Vector3 offSet;
 
     EnemyCombat ec;
     Rigidbody2D rig;
 
     void Start ()
     {
+        target = transform;
+        offSet = Vector3.zero;
         ec = GetComponent<EnemyCombat>();
         rig = GetComponent<Rigidbody2D>();
         rig.gravityScale = 0;
@@ -20,7 +26,25 @@ public class FlyingEnemyMovement : MonoBehaviour {
     {
 		if(!ec.dead)
         {
+            if(ec.seePlayer)
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+                offSet.y += 1.15f; //adding so target is center of player
+            }
+            else
+            {
+                target = transform;
+                offSet = Vector3.zero;
+            }
 
+            //--Movement-----------------------------------------------------------------------------
+            Vector2 processVelocity = transform.InverseTransformDirection(rig.velocity);
+            if ((ec.seePlayer && !ec.AlwaysMove) || (ec.AlwaysMove && !ec.attacking))
+            {
+
+            }
+            rig.velocity = transform.TransformDirection(processVelocity);
         }
+
 	}
 }
