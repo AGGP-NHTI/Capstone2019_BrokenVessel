759ba67818babfc8da5f51ad738acfc5647ff62f
Raus <rausyrasu@gmail.com>
2019-02-26 15:46:13 -0500

    Fixed floor collisions and optimized calls

--- Assets/Scripts/Player/PlayerPhysics.cs
+++ Assets/Scripts/Player/PlayerPhysics.cs
@@ -49,8 +49,7 @@ namespace BrokenVessel.Player
 			velocity.y = Mathf.Max(velocity.y, -terminalVelocity); // Cap to terminal velocity
 
 			// Check floor
-			float dist;
-			if (CheckFloor(out dist))
+			if (grounded = CheckFloor(out float dist))
 			{
 				pos.y -= dist; // Snap to floor
 				velocity.y = 0; // Remove gravity
@@ -66,12 +65,12 @@ namespace BrokenVessel.Player
 
 		public void Jump()
 		{
-			if (CheckFloor()) { velocity.y = jumpStrength; }
+			if (grounded) { velocity.y = jumpStrength; }
 		}
 
 		public void HalveJump()
 		{
-			if (velocity.y > 0 && halvedJump) { velocity.y /= 2f; halvedJump = true; }
+			if (velocity.y > 0 && !halvedJump) { velocity.y /= 2f; halvedJump = true; }
 		}
 
 		public void Move(float dir)
@@ -79,7 +78,7 @@ namespace BrokenVessel.Player
 			// Apply friction
 			if (dir == 0 && velocity.x != 0 || Mathf.Abs(velocity.x) > maxSpeed)
 			{
-				float fric = (CheckFloor() ? groundFriction : airFriction) * Time.deltaTime;
+				float fric = (grounded ? groundFriction : airFriction) * Time.deltaTime;
 
 				velocity.x -= fric * Mathf.Sign(velocity.x);
 				
@@ -90,8 +89,8 @@ namespace BrokenVessel.Player
 				}
 			}
 
-			float newVelX = velocity.x + dir * (CheckFloor() ? groundSpeed : airSpeed) * Time.deltaTime;
-			if (Mathf.Abs(velocity.x) < maxSpeed)
+			float newVelX = velocity.x + dir * (grounded ? groundSpeed : airSpeed) * Time.deltaTime;
+			if (Mathf.Abs(velocity.x) <= maxSpeed)
 			{
 				velocity.x = Mathf.Clamp(newVelX, -maxSpeed, maxSpeed); // Cap to max speed
 			}
@@ -111,8 +110,11 @@ namespace BrokenVessel.Player
 		private bool CheckFloor(out float dist)
 		{
 			float half = box.size.y / 2f;
+			float quarter = box.size.y / 4f;
+			Vector2 size = box.size;
+			size.y /= 2f;
 			
-			RaycastHit2D hit = Physics2D.BoxCast(transform.position + Vector3.up * half, box.size, 0, Vector2.down, -velocity.y * Time.deltaTime + half, collisionMask);
+			RaycastHit2D hit = Physics2D.BoxCast(transform.position + Vector3.up * quarter, size, 0, Vector2.down, -velocity.y * Time.deltaTime + half, collisionMask);
 
 			dist = hit.distance - half;
 
