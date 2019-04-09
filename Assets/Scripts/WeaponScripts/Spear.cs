5d10d7095787d1105f8d615f13936416ce7395df
Hyperion47 <37087822+Hyperion47@users.noreply.github.com>
2019-04-09 16:50:07 -0400

    changed the spear so that it aims

--- Assets/Scripts/WeaponScripts/Spear.cs
+++ Assets/Scripts/WeaponScripts/Spear.cs
@@ -2,36 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spear : Weapon
-{
+public class Spear : Weapon {
 
-    // Use this for initialization
-    void Start () {
-        
-    }
+    Vector3 mouse;
 
-    // Update is called once per frame
-    public override void Update() {
-        base.Update();
-        if (HitBox == null)
-        {
-            HitBox = GameObject.Find("SpearHitBox");
-        }
+
+    void Update ()
+    {
+        mouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) - transform.position;
+        transform.rotation = Quaternion.Euler(Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg, -90, 0);
     }
 
     public override void Attack()
     {
-        StartCoroutine(SwingSpear());
+        StartCoroutine(PushSpear());
         base.Attack();
     }
 
-    public IEnumerator SwingSpear()
+    public IEnumerator PushSpear()
     {
         yield return new WaitForSeconds(.1f);
-        HitBox.GetComponent<BoxCollider2D>().enabled = true;
-        gameObject.transform.Translate(0, 0, -2f);
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        gameObject.transform.Translate(0, 0, -1f);
         yield return new WaitForSeconds(.1f);
-        gameObject.transform.Translate(0, 0, 2f);
-        HitBox.GetComponent<BoxCollider2D>().enabled = false;
+        gameObject.transform.localPosition = new Vector3(0,1,0);
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
     }
 }
