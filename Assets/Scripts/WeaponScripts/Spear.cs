21b91a81f4380e69bbeaff7f8383e87b22ae677a
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2019-04-12 15:25:23 -0400

    THE REAL SPEAR FIX

--- Assets/Scripts/WeaponScripts/Spear.cs
+++ Assets/Scripts/WeaponScripts/Spear.cs
@@ -11,6 +11,7 @@ public class Spear : Weapon {
     {
         mouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) - transform.position;
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg);
+        transform.localScale = transform.parent.localScale;
     }
 
     public override void Attack()
