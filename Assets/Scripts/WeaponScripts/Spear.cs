edcabc8ea539e9bee95ff2e2ec90c49adcf49174
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2019-04-12 14:42:59 -0400

    NewSpear

--- Assets/Scripts/WeaponScripts/Spear.cs
+++ Assets/Scripts/WeaponScripts/Spear.cs
@@ -10,7 +10,7 @@ public class Spear : Weapon {
     void Update ()
     {
         mouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) - transform.position;
-        transform.rotation = Quaternion.Euler(Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg, -90, 0);
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg);
     }
 
     public override void Attack()
