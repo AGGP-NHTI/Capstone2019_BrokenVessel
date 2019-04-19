0a0aab8bfb5429a688af03c4b58266f4a44826d8
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2019-04-19 15:44:29 -0400

    starting again on enemy drops and picksup

---
+++ Assets/Scripts/ItemScripts/Consumable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BrokenVessel.Interact
+{
+    public class Consumable : Interact
+    {
+        public string type = "";
+        public int amount = 0;
+        public override void Impulse()
+        {
+
+        }
+    }
+}
