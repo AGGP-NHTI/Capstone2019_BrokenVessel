d6d2c5b46fc785b43c432b81ab057e30b38a0343
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2019-04-23 14:48:20 -0400

    enemies can no longer drop empty value items

--- Assets/Scripts/EnemyScripts/EnemyDrops.cs
+++ Assets/Scripts/EnemyScripts/EnemyDrops.cs
@@ -22,16 +22,19 @@ public class EnemyDrops : MonoBehaviour
         if (isAmountRandom) { Amount = (int)Random.Range(range.x, range.y); }
 
         GameObject temp;
-        switch(dropType)
+        if (Amount > 0)
         {
-            case Drops.health:
-                temp = Instantiate(healthPickUp, transform.position, Quaternion.identity) as GameObject;
-                temp.GetComponent<Consumable>().amount = Amount;
-                break;
-            case Drops.scrap:
-                temp = Instantiate(scrapPickUp, transform.position, Quaternion.identity) as GameObject;
-                temp.GetComponent<Consumable>().amount = Amount;
-                break;
+            switch (dropType)
+            {
+                case Drops.health:
+                    temp = Instantiate(healthPickUp, transform.position, Quaternion.identity) as GameObject;
+                    temp.GetComponent<Consumable>().amount = Amount;
+                    break;
+                case Drops.scrap:
+                    temp = Instantiate(scrapPickUp, transform.position, Quaternion.identity) as GameObject;
+                    temp.GetComponent<Consumable>().amount = Amount;
+                    break;
+            }
         }
     }
 }
