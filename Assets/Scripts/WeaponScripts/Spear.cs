0d02a64cf3ba44b7eb1d9f1247ac4971b18d7460
KyleWelch00 <37556482+KyleWelch00@users.noreply.github.com>
2019-04-26 14:33:32 -0400

    Spear Dead Zone Added

--- Assets/Scripts/WeaponScripts/Spear.cs
+++ Assets/Scripts/WeaponScripts/Spear.cs
@@ -44,24 +44,9 @@ public class Spear : Weapon
     {
         r1 = Input.GetAxis("RightHorizontal");
         r2 = Input.GetAxis("RightVertical");
-        if (r1 == 0f && r2 == 0f)
-        { // this statement allows it to recenter once the inputs are at zero 
-            Vector3 curRot = this.transform.localEulerAngles; // the object you are rotating
-            Vector3 homeRot;
-            if (curRot.y > 180f)
-            { // this section determines the direction it returns home 
-                Debug.Log(curRot.y);
-                homeRot = new Vector3(0f, 90f, 0f); //it doesnt return to perfect zero 
-            }
-            else
-            {                                                                      // otherwise it rotates wrong direction 
-                homeRot = Vector3.zero;
-            }
-           transform.rotation = Quaternion.Euler(Vector3.Slerp(curRot, homeRot, Time.deltaTime * 2));
-        }
-        else
+        if (r1 >= 0.3f || r2 >= 0.3f)
         {
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (Mathf.Atan2(r1, r2)-90) * 180 / Mathf.PI)); // this does the actual rotaion according to inputs
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (Mathf.Atan2(r1, r2) - 90) * 180 / Mathf.PI)); // this does the actual rotaion according to inputs
         }
     }
 }
