efe5ea162f2cc37a29d50e14518e52897e061dd5
KyleWelch00 <37556482+KyleWelch00@users.noreply.github.com>
2019-04-30 14:41:24 -0400

    gun no dead spot

--- Assets/Scripts/WeaponScripts/RangedWeapon.cs
+++ Assets/Scripts/WeaponScripts/RangedWeapon.cs
@@ -13,11 +13,11 @@ public class RangedWeapon : Weapon
     [SerializeField] Transform barrelEnd;
     [SerializeField] GameObject PlayerBullet;
     [SerializeField] float projectileSpeed;
- 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -66,22 +66,8 @@ public class RangedWeapon : Weapon
     {
         r1 = Input.GetAxis("RightHorizontal");
         r2 = Input.GetAxis("RightVertical");
-        if (r1 == 0f && r2 == 0f)
-        { // this statement allows it to recenter once the inputs are at zero 
-            Vector3 curRot = this.transform.localEulerAngles; // the object you are rotating
-            Vector3 homeRot;
-            if (curRot.y > 180f)
-            { // this section determines the direction it returns home 
-                Debug.Log(curRot.y);
-                homeRot = new Vector3(0f, 90f, 0f); //it doesnt return to perfect zero 
-            }
-            else
-            {                                                                      // otherwise it rotates wrong direction 
-                homeRot = Vector3.zero;
-            }
-            transform.rotation = Quaternion.Euler(Vector3.Slerp(curRot, homeRot, Time.deltaTime * 2));
-        }
-        else
+        Vector2 aim = new Vector2(r1, r2);
+        if (aim.magnitude > 0.2f)
         {
             transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (Mathf.Atan2(r1, r2) - 90) * 180 / Mathf.PI)); // this does the actual rotaion according to inputs
         }
