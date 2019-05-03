9d688101686a78c239987fcdc8caccb9be474daa
JacobDusseault <37554244+JacobDusseault@users.noreply.github.com>
2019-05-03 15:13:08 -0400

    Broken Ceiling Light Done

--- Assets/Scripts/CelingLightControl.cs
+++ Assets/Scripts/CelingLightControl.cs
@@ -9,86 +9,95 @@ public class CelingLightControl : MonoBehaviour
     public Material Lightoff_mat;
     public GameObject LightObject;
     public GameObject particleObject;
-    private float flickerMax = 5;
-    private float flickerMin = 0;
-    private float flickerRange;
-    WaitForSeconds WFS;
+    public float flickerMax = 2f;
+    public float flickerMin = .5f;
+    float flickerRange = 0;
+    float flickercount = 0;
+    public bool IsLightOn = true; 
+
     Renderer render;
 
     public virtual void Start()
     {
         render = gameObject.GetComponent<Renderer>();
+        flickerRange = Random.Range(flickerMin, flickerMax);
+        flickercount = 0;
     }
 
    
     public virtual void Update()
     {
-        flickerRange = Random.Range(flickerMax, flickerMin);
+        flickercount += Time.deltaTime;
 
-        if (flickerRange > flickerMax / 2)
+
+        if (flickercount > flickerRange)
         {
-            LightOn();
-            WFS = new WaitForSeconds(2);
+            if (IsLightOn)
+            {
+                LightOff();
+            }
+            else
+            {
+                LightOn();
+            }
+            flickerRange = Random.Range(flickerMin, flickerMax);
+            IsLightOn = !IsLightOn;
+            flickercount = 0;
         }
-        if(flickerRange < flickerMax / 2)
-        {
-            LightOff();
-            WFS = new WaitForSeconds(2);
-        }
+
     }
 
     public virtual void LightOn()
     {
-        if (LightObject == null || particleObject == null)
+        if (!LightObject || !particleObject)
         {
             Debug.Log("CeilingLightControl: Can't find the LightObject or particleObject");
             return;
         }
-        else if (LightObject == true)
-        {
-            Debug.Log("CeilingLightControl: LightObject is already set to true");
-            return;
-        }
-        else if (particleObject == false)
-        {
-            Debug.Log("CeilingLightControl: particleObject is already set to false");
-            return;
-        }
-        else
-        {
-            Material[] mats = render.materials;
-            mats[2] = LightOn_mat;
-            render.materials = mats;
-            LightObject.SetActive(true);
-            particleObject.SetActive(false);
-        }
-    }
-
-    public virtual void LightOff()
-    {
-        if (LightObject == null || particleObject == null)
-        {
-            Debug.Log("CeilingLightControl: Can't find the LightObject or particleObject");
-            return;
-        }
-        else if (LightObject == false)
+        if (!LightObject)
         {
             Debug.Log("CeilingLightControl: LightObject is already set to false");
             return;
         }
-        else if (particleObject == true)
+        if (!particleObject)
         {
             Debug.Log("CeilingLightControl: particleObject is already set to true");
             return;
         }
-        else
+
+        Material[] mats = render.materials;
+        mats[2] = LightOn_mat;
+        render.materials = mats;
+        LightObject.SetActive(true);
+        particleObject.SetActive(false);
+
+    }
+
+    public virtual void LightOff()
+    {
+        if (!LightObject || !particleObject)
         {
+            Debug.Log("CeilingLightControl: Can't find the LightObject or particleObject");
+            return;
+        }
+        if (!LightObject)
+        {
+            Debug.Log("CeilingLightControl: LightObject is already set to false");
+            return;
+        }
+        if (!particleObject)
+        {
+            Debug.Log("CeilingLightControl: particleObject is already set to true");
+            return;
+        }
+        
+        
             Material[] mats = render.materials;
             mats[2] = Lightoff_mat;
             render.materials = mats;
             LightObject.SetActive(false);
             particleObject.SetActive(true);
-        }
+        
     }
 
 
