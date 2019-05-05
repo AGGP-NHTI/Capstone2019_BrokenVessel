ae16e902f0d1b49d5e1cb929a0c0efe666d45df9
GearedAtFace <37555402+GearedAtFace@users.noreply.github.com>
2019-05-05 18:30:39 -0400

    Boss room platform and moving textures

--- Assets/Scripts/MovingTexture.cs
+++ Assets/Scripts/MovingTexture.cs
@@ -6,6 +6,7 @@ public class MovingTexture : MonoBehaviour
 {
     public int materialIndex = 0;
     public Vector2 uvAnimationRate = new Vector2(0.0f, 1.0f);
+    public Vector2 tiling = new Vector2(1.0f, 1.0f);
     public string textureName = "_MainTex";
 
 
@@ -16,7 +17,7 @@ public class MovingTexture : MonoBehaviour
         if (GetComponent<Renderer>().enabled)
         {
             GetComponent<Renderer>().materials[materialIndex].SetTextureOffset(textureName, uvOffset);
-            GetComponent<Renderer>().materials[materialIndex].mainTextureScale = new Vector2(0, 25);
+            GetComponent<Renderer>().materials[materialIndex].mainTextureScale = tiling;
         }
     }
 }
