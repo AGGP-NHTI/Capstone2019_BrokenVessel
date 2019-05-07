41df081a02553c13657b4f9210df1c39bc9ec32f
GearedAtFace <37555402+GearedAtFace@users.noreply.github.com>
2019-05-06 21:34:29 -0400

    Misc changes from list on trello

---
+++ Assets/Scripts/ButtonPrompt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPrompt : MonoBehaviour
+{
+
+    public GameObject textBox;
+    public Transform spawnPosition;
+    public float triggerDistance = 3.0f;
+    GameObject Player;
+    GameObject textBoxInstance;
+    bool textBoxVisible;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(Player.transform.position, gameObject.transform.position) < triggerDistance)
+        {
+            if (textBoxVisible == false)
+            {
+                textBoxInstance = Instantiate(textBox, spawnPosition);
+                textBoxVisible = true;
+            }
+        }
+        else
+        {
+            if (textBoxVisible == true)
+            {
+                Destroy(textBoxInstance);
+                textBoxVisible = false;
+            }
+        }
+    }
+}
